6c2175b17cb3cc08ce31e99af8a18d38f238d0f5
JamesMokua <jamieedgar02@gmail.com>
2023-09-16 23:14:03 +0300

    drizzle drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
 import { LogInIcon } from "lucide-react";
+import FileUpload from "@/components/ui/FileUpload";
 
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
 
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100">
+    <div className="w-screen min-h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-100 via-blue-300 to-blue-500">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 ">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <h1 className="mr-3 text-5xl font-semibold ">Analyse your PDF</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
           <div className="flex mt-2">
@@ -25,7 +26,7 @@ export default async function Home() {
           </p>
           <div className="w-full mt-4">
             {isAuth ? (
-              <h1>fileUPload</h1>
+              <FileUpload/>
             ) : (
               <Link href="/sign-in">
                 <Button>
