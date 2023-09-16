fa0c586072a5115d8b687e3896e35d3b3401a129
JamesMokua <jamieedgar02@gmail.com>
2023-09-16 17:58:00 +0300

    Home page and clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,42 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogInIcon } from "lucide-react";
 
 export default async function Home() {
-  return <Button>Click me</Button>;
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 ">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of people who use somaPDF to read documents, answer
+            questions and understand resources
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileUPload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get started
+                  <LogInIcon className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
